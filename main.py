44a173262ce7f3df7dcffdee50a67611f9fd7281
Meshgrad <svkaplunov81@gmail.com>
2025-12-19 00:46:30 +0300

    try-except 2

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-blog_posts = [{'Photos': 3, 'Likes': 21, 'Comments': 2}, 
-              {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
-              {'Photos': 5, 'Likes': 33, 'Comments': 8, 'Shares': 3}, 
-              {'Comments': 4, 'Shares': 2}, 
-              {'Photos': 8, 'Comments': 1, 'Shares': 1}, 
-              {'Photos': 3, 'Likes': 19, 'Comments': 3}]
+food = ['chocolate', 'chicken', 'corn', 'sandwich', 'soup', 'potatoes', 'beef', 'lox', 'lemonade']
+fifth = []
 
-total_likes = 0
-
-for post in blog_posts:
+for x in food:
     try:
-        total_likes += post['Likes']
+        fifth.append(x[4])
     except:
-        total_likes -= 1
+        fifth.append('_')
 
-print(total_likes)
+print(fifth)
