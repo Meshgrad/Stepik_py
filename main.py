a5e4a3132c5c93200f1d6bc63b9a56c0e8cb0d10
Meshgrad <svkaplunov81@gmail.com>
2025-12-25 21:38:16 +0300

    7.5 is_good_password 2

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-import string
+from string import punctuation
+class PasswordError(Exception):
+    pass
 
+class LengthError(PasswordError):
+    pass
+
+class LetterError(PasswordError):
+    pass
+
+class DigitError(PasswordError):
+    pass
 
 def is_good_password(string):
-    if not (len(string) >= 9):
-        return False
-    if not any(i.is_digit() for i in string):
-           return False
-    if not (string != string.lower() and string != string.upper()):
-        return False
-    return True
-              
+    try:
+        if len(string) < 9: raise LengthError
+        if not (string != string.lower() and string != string.upper() and not (any(x in punctuation for x in string))): raise LetterError
+        if not any(i.isdigit() for i in string): raise DigitError
+        
+    except LengthError:
+        raise
+    except LetterError:
+        raise
+    except DigitError:
+        raise 
+    
+    
+try:
+    print(is_good_password('!@#$%^&*()_+'))
+except Exception as err:
+    print(type(err))
