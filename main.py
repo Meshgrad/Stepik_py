fbe045fc82d528599636de26167d77daeabdc48b
Meshgrad <svkaplunov81@gmail.com>
2025-12-25 23:08:08 +0300

    recursive

--- main.py
+++ main.py
@@ -1,41 +1,8 @@
-from string import punctuation
-class PasswordError(Exception):
-    pass
 
-class LengthError(PasswordError):
-    pass
 
-class LetterError(PasswordError):
-    pass
-
-class DigitError(PasswordError):
-    pass
-
-def is_good_password(string):
-    try:
-        string[8]
-    except IndexError:
-        raise LengthError
-
-    try:
-        list(filter(str.isupper, string))[0]
-        list(filter(str.islower, string))[0]
-    except IndexError:
-        raise LetterError
-
-    try:
-        list(filter(str.isdigit, string))[0]
-    except IndexError:
-        raise DigitError
-
-    return True
-
-while True:
-    try:
-        is_good_password(input())
-    except Exception as err:
-        print(err.__class__.__name__)
-    else:
-        print("Success!")
-        break
+def hundred(num):
+    if num <= 100:
+        print(num)
+        hundred(num + 1)
+hundred(1)
 
