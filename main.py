cdfa1dc70a011d05528cdd8d73b56d80611caacd
Meshgrad <svkaplunov81@gmail.com>
2025-12-26 00:02:44 +0300

    sand recurs

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
-def triangle(num):
-    if num > 0:
-        print(num * '*')
-        triangle(num - 1)    
-        
+
+
+def sand_clock():
+    def st(num, step):
+        print(f'{str(step) * num}'.center(16))
+        if step < 4:
+            st(num - 4, step + 1)
+            print(f'{str(step) * num}'.center(16))  
+    st(16, 1)
+sand_clock()
