e9359d665a11c0d31d4d25b07a26ffc1bddc3424
Meshgrad <svkaplunov81@gmail.com>
2025-12-31 01:31:29 +0300

    recurs 8.3 is_palindrom

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-
-
-def is_power(number):
-    if number < 2:
-        if number == 1:
-            return True
-        else:
-            return False
+def is_palindrome(string):
+    if string == '' or len(string) == 1:
+        return True
+    if string[0] != string [-1]:
+        return False
     else:
-        return is_power(number / 2)
-
-print(is_power(512))
+        return is_palindrome(string[1:-1])
+        
+print(is_palindrome('122333221'))
