bd3d7fe39af7eb42668f79d1e7cfadf9419c9a8f
Meshgrad <svkaplunov81@gmail.com>
2025-12-31 00:27:50 +0300

    recurs 8.3 -3

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 
 
-def recurs(num):
-    res = 0
-    if len(num) == 1:
-        return 1
-    else:
-        return 1 + recurs(num[1:])
-    
-print(recurs(input()))
+def number_of_frogs(year):
+    if year == 1:
+        return 77
+    return 3*(number_of_frogs(year - 1) - 30)
+
+print(number_of_frogs(10))
