373d80a612abb4a331f36ed91452504cc38ba06d
Meshgrad <svkaplunov81@gmail.com>
2025-12-31 23:52:20 +0300

    recurs 8.3 commit from 2025))

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def to_binary(number):
-    if int(number) == 0:
-        return 0
-    elif int(number) == 1:
-        return 1
-    else:
-        return str(to_binary((number) // 2)) + str(int(number) % 2)
-        
+
+def bee(n):
+    if n > 0:
+        print(n)
+        bee(n - 5)
+    print(n)
+
+bee(16)
