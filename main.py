bdb6036910fd3b68ef3a4c3353fefb74d16d949a
Meshgrad <svkaplunov81@gmail.com>
2026-01-02 21:43:20 +0300

    dict_travel

--- main.py
+++ main.py
@@ -1,31 +1,14 @@
 
-def get_all_values(nested_dicts, key):
-    total = set()
-    def rec(nested_dicts, key):
-        if type(nested_dicts) == dict and key in nested_dicts:
-            total.add(nested_dicts[key])
+def dict_travel(nested_dicts):
+    def rec(nested_dicts, fold=[]):
+        if type(nested_dicts) != dict:
+            print(f"{'.'.join(fold)}: {nested_dicts}")
         if type(nested_dicts) == dict:
-            
-            for v in nested_dicts.values():
-                value = rec(v, key)
-                #total.update(value)
-                #return total
-    rec(nested_dicts, key)
-    return total
-    
-my_dict = {
-           'Arthur': {'hobby': 'videogames', 'drink': 'cacao'}, 
-           'Timur': {'hobby': 'math'},
-           'Dima': {
-                   'hobby': 'CS',
-                   'sister':
-                       {
-                         'name': 'Anna',
-                         'hobby': 'TV',
-                         'age': 14
-                       }
-                   }
-           }
+            for k, v in sorted(nested_dicts.items()):
+                rec(v, fold + [k])
+    rec(nested_dicts, [])
+    return
 
-result = get_all_values(my_dict, 'hobby')
-print(*sorted(result))
+data = {'b': {'c': 30, 'a': 10, 'b': {'d': 40, 'e': 50}}}
+
+dict_travel(data)
