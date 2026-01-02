8d5a3af90d3636143e98f92b9a3542cead9a9c82
Meshgrad <svkaplunov81@gmail.com>
2026-01-03 02:31:25 +0300

    func 9.1

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 
-def dict_travel(nested_dicts):
-    def rec(nested_dicts, fold=[]):
-        if type(nested_dicts) != dict:
-            print(f"{'.'.join(fold)}: {nested_dicts}")
-        if type(nested_dicts) == dict:
-            for k, v in sorted(nested_dicts.items()):
-                rec(v, fold + [k])
-    rec(nested_dicts, [])
-    return
 
-data = {'b': {'c': 30, 'a': 10, 'b': {'d': 40, 'e': 50}}}
+def convert(number):
+    return (format(number, 'b'), '%o' % number, '%X' % number)
 
-dict_travel(data)
+print(convert(-24))
