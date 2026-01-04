7401dd45958174a6fad88aad99eab7af53d2727f
Vladislav <svkaplunov81@gmail.com>
2026-01-05 01:56:38 +0300

    9.1 last sort

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-names = ['Moana', 'Cars', 'Zootopia', 'Ratatouille', 'Coco', 'Inside Out', 'Finding Nemo', 'Frozen']
-budgets = [150000000, 120000000, 150000000, 150000000, 180000000, 175000000, 94000000, 150000000]
-box_offices = [643331111, 462216280, 1023784195, 620702951, 807082196, 857611174, 940335536, 1280802282]
 
 
-
-
+def sorting(string:str):
+    a, b, c = [], [], []
+    for ch in string:
+        if ch == ch.lower() and not ch.isdigit():
+            a.append(ch)
+        elif ch == ch.upper() and not ch.isdigit():
+            b.append(ch)
+        elif ch.isdigit():
+            c.append(ch)
+    return ''.join(sorted(a) + sorted(b) + sorted(c, key=lambda x: (int(x) % 2 == 0, int(x))))
+    
+    
+print(sorting(input()))
