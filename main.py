1aa5684b9198978ec1ba027878c765f3c4a24e16
Vladislav <svkaplunov81@gmail.com>
2026-01-06 21:13:14 +0300

    remove_marks

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
-import string
-
-d = {0: 'в пароле нет ни одной буквы',
-         1: 'в пароле нет ни одной заглавной буквы',
-         2: 'в пароле нет ни одной строчной буквы',
-         3: 'в пароле нет ни одной цифры'}
-
-def verification(login, password, success, failure):
-    if not any([x in string.ascii_letters for x in password]):
-        failure(login, d[0])
-        return
-    if not any([x in string.ascii_uppercase for x in password]):
-        failure(login, d[1])
-        return
-    if not any([x in string.ascii_lowercase for x in password]):
-        failure(login, d[2])
-        return
-    if not any([x in string.digits for x in password]):
-        failure(login, d[3])
-        return
-    success(login)
-
-def success(login):
-    print(f'Здравствуйте, {login}!')
 
 
-def failure(login, text):
-    print(f'{login}, попробуйте снова. Текст ошибки: {text}')
+def remove_marks(text:str, marks:str):
+    remove_marks.__dict__.setdefault('count', 0)
+    remove_marks.count += 1
+    
+    res = text
+    for mark in marks:
+        res = res.replace(mark, '')
+    return res
+    
+    
+text = 'Hi! Will we go together?'
 
-
-verification('Arthur_Davletov', 'мойпароль123', success, failure)
+print(remove_marks(text, '!?'))
+print(remove_marks.count)
