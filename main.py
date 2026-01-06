dd1d4f5c90f0a5a682b65ae33a52f573d37a40e7
Vladislav <svkaplunov81@gmail.com>
2026-01-06 18:51:39 +0300

    recurs anonim func

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 
 
-def sorting(string:str):
-    a, b, c = [], [], []
-    for ch in string:
-        if ch == ch.lower() and not ch.isdigit():
-            a.append(ch)
-        elif ch == ch.upper() and not ch.isdigit():
-            b.append(ch)
-        elif ch.isdigit():
-            c.append(ch)
-    return ''.join(sorted(a) + sorted(b) + sorted(c, key=lambda x: (int(x) % 2 == 0, int(x))))
-    
-    
-print(sorting(input()))
+fib = lambda x: 1 if x <= 2 else fib(x - 1) + fib(x - 2)
+
+print(fib(5))
