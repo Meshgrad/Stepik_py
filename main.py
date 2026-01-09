4cfa80e35e4c397dcefbc6e86de5ab6473eb1b33
Vladislav <svkaplunov81@gmail.com>
2026-01-09 03:24:21 +0300

    matrix_to_dict()

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 
 
-def sourcetemplate(query_string):
-    def ld(**kwargs):
-        nonlocal query_string
-        if len(kwargs) == 0:
-            return query_string
-        else:
-            query_string += '?'
-            for key, value in sorted(kwargs.items()):
-                query_string+= f"&{key}={value}"
-        return query_string
-    return ld
+def matrix_to_dict(matrix: list[list[int | float]], ) -> dict[int, list[int | float]]:
+    return {idx: value for idx, value in enumerate(matrix, 1)}
+annotations = matrix_to_dict.__annotations__
 
-url = 'https://all_for_comfort_life.com'
-load = sourcetemplate(url)
-print(load(smartphone='iPhone', notebook='huawei', sale=True))
+print(annotations['return'])
