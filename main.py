be6dfe8a71aeffd616ba6c70999eca010298ddf0
Vladislav <svkaplunov81@gmail.com>
2026-01-14 23:48:04 +0300

    Iterator 10.3 part 3 random_numbers()

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import copy 
+def get_min_max(data):
+    try:
+        collect = iter(data)
+        mn = mx = next(collect)
+        for i in collect:
+            if i > mx: mx = i
+            if i < mn: mn = i
+        return mn, mx
+    except:
+        pass
+
+iterable = iter(range(10))
+
+print(get_min_max(iterable))
 
 
-def filterfalse(predicate, iterable):
-    if predicate != None:
-        return filter(lambda x:  not predicate(x), iterable)
-    else:
-        return filter(lambda x:  not bool(x), iterable)
-
-numbers = (1, 2, 3, 4, 5)
-
-print(*filterfalse(lambda x: x % 2 == 0, numbers))
